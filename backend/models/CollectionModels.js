1a6851c6cb46a6e4bc07e56adad8296335dabb4c
pasham Revanth <pashamrevanth541@gmail.com>
2026-01-30 23:27:46 +0530

    defining the Models with Typescripts

--- backend/models/CollectionModels.js
+++ backend/models/CollectionModels.js
@@ -1,11 +1,20 @@
-// const mongoose=require("mongoose")
-// const Collections= new mongoose.Schema(
-//     {   
-//         imageUrl:{type:String,required:true},
-//         category:{type:[String],required:true},
-//         discription:{type:String,required:true},
-//         name:{type:String,required:true}  
-//     }
-// )
-// const CollectionsModel= mongoose.model("Collections",Collections)
-// module.exports={CollectionsModel}
+import { model, Schema, Document } from "mongoose";
+
+export interface ICollection extends Document {
+  name: string;
+  imageUrl: string;
+  category: string[];
+  description: string;
+}
+
+const CollectionsSchema = new Schema<ICollection>(
+  {
+    name: { type: String, required: true, trim: true },
+    imageUrl: { type: String, required: true },
+    category: { type: [String], required: true },
+    description: { type: String, required: true }
+  },
+  { timestamps: true }
+);
+
+export default model<ICollection>("Collections", CollectionsSchema);

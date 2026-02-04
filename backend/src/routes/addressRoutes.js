c5c7033fb5644c8723a1cf1953c7dac5d8771571
pasham Revanth <pashamrevanth541@gmail.com>
2026-02-04 17:24:24 +0530

    removing code sniet

--- backend/src/routes/addressRoutes.js
+++ backend/src/routes/addressRoutes.js
@@ -1,10 +1,10 @@
-const express= require("express");
-const router= express.Router();
+// const express= require("express");
+// const router= express.Router();
 
-const {addAddress,getAdddress,updateAddress,deleteAddress} = require("../controllers/addressControllers")
-router.post("/add",addAddress)
-router.get("/add/:userId",getAdddress)
-router.put("/add/:userId",updateAddress)
-router.delete("/add/:userId",deleteAddress)
+// const {addAddress,getAdddress,updateAddress,deleteAddress} = require("../controllers/addressControllers")
+// router.post("/add",addAddress)
+// router.get("/add/:userId",getAdddress)
+// router.put("/add/:userId",updateAddress)
+// router.delete("/add/:userId",deleteAddress)
 
-module.exports= router;
+// module.exports= router;
--- backend/src/routes/cartRoutes.js
+++ backend/src/routes/cartRoutes.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const router = express.Router();
+// const express = require("express");
+// const router = express.Router();
 
-const {
-  addToCart,
-  getCartByUser,removeFromCart
-} = require("../controllers/cartController"); // 👈 CHECK THIS PATH
-router.post("/add", addToCart);
-router.get("/:userId", getCartByUser);
-router.delete("/:userId/:productId",removeFromCart);
+// const {
+//   addToCart,
+//   getCartByUser,removeFromCart
+// } = require("../controllers/cartController"); // 👈 CHECK THIS PATH
+// router.post("/add", addToCart);
+// router.get("/:userId", getCartByUser);
+// router.delete("/:userId/:productId",removeFromCart);
 
-module.exports = router;
+// module.exports = router;

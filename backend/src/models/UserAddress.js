94e4c57a291a811f813acb987b7a9982fd012347
pasham Revanth <pashamrevanth541@gmail.com>
2026-02-04 17:22:46 +0530

    removing code sniet

--- backend/src/models/UserAddress.js
+++ backend/src/models/UserAddress.js
@@ -1,42 +1,42 @@
-const mongoose = require("mongoose");
-const addressSchema = new mongoose.Schema(
-  {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",   
-      required: true
-    },
-    name: {
-      type: String,
-      required: true
-    },
+// const mongoose = require("mongoose");
+// const addressSchema = new mongoose.Schema(
+//   {
+//     userId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "User",   
+//       required: true
+//     },
+//     name: {
+//       type: String,
+//       required: true
+//     },
 
-    email: {
-      type: String,
-      required: true
-    },
-    phone: {
-      type: Number,
-      required: true
-    },
-    city: {
-      type: String,
-      required: true
-    },
-    pincode: {
-      type: Number,
-      required: true
-    },
+//     email: {
+//       type: String,
+//       required: true
+//     },
+//     phone: {
+//       type: Number,
+//       required: true
+//     },
+//     city: {
+//       type: String,
+//       required: true
+//     },
+//     pincode: {
+//       type: Number,
+//       required: true
+//     },
 
-    address: {
-      type: String,
-      required: true
-    },
-    reference: {
-      type: String
-    }
-  },
-  { timestamps: true }
-);
-const Address = mongoose.model("Address", addressSchema);
-module.exports = Address;
+//     address: {
+//       type: String,
+//       required: true
+//     },
+//     reference: {
+//       type: String
+//     }
+//   },
+//   { timestamps: true }
+// );
+// const Address = mongoose.model("Address", addressSchema);
+// module.exports = Address;

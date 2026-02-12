881d73a8fcc159587cbe6a4515996dd5702df364
pasham Revanth <pashamrevanth541@gmail.com>
2026-02-12 23:23:07 +0530

    deleted dublicates

--- backend/src/routes/CollectionRoutes.ts
+++
@@ -1,9 +0,0 @@
-// import { Router } from "express";
-// import { getCollections } from "../controllers/collectionControllers";
-
-// const router = Router();
-
-// router.get("/collections", getCollections);
-// router.get("/Collections", getCollections);
-
-// export default router;

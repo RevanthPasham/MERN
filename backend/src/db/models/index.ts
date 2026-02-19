23e3ab83c96c0d024f65339b33b044de54880c35
pasham Revanth <pashamrevanth541@gmail.com>
2026-02-19 23:53:59 +0530

    deleted models controllers and more to restart the archtechure

--- backend/src/db/models/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./category.model";
-export * from "./product.model";
-export * from "./productVariant.model";
-export * from "./productImage.model";
-export * from "./collection.model";
-export * from "./init";
--- backend/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import categoryRoutes from "./category.routes";
-
-const router = Router();
-
-router.use("/categories", categoryRoutes);
-
-export default router;

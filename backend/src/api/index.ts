5a4e2d87533e05dbd68f910cba5b310674f657b7
pasham Revanth <pashamrevanth541@gmail.com>
2026-03-03 16:55:03 +0530

    changed folder structure

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,6 +1,6 @@
 import serverless from "serverless-http";
-import app from "../src/app";
-import { sequelize } from "../src/config/db";
+import app from "../app";
+import { sequelize } from "../config/db";
 
 let isConnected = false;
 

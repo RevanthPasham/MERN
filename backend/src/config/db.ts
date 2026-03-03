43190964788486f1e3ff65d27dcfc8508dfe0e9e
pasham Revanth <pashamrevanth541@gmail.com>
2026-03-03 17:42:22 +0530

    checking deployment logs

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,9 +1,11 @@
-import { Sequelize } from "sequelize";
-import pg from "pg";
+]import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL missing");
+}
+
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
-  dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true,

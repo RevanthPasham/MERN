69b1c385fb6d2ad88968acb09c5ce59b4c97b43c
pasham Revanth <pashamrevanth541@gmail.com>
2026-03-03 17:54:05 +0530

    removed root directlory in tsconfig

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,19 +1,5 @@
-import serverless from "serverless-http";
-import app from "../src/app";
-import { initModels } from "../src/db/models";
 
-let initialized = false;
 
-async function init() {
-  if (!initialized) {
-    await initModels();
-    initialized = true;
-  }
+export default function handler(req: any, res: any) {
+  res.status(200).send("API WORKING");
 }
-
-const handler = async (req: any, res: any) => {
-  await init();
-  return serverless(app)(req, res);
-};
-
-export default handler;

149183cc324988680a8db8c50e53df130c1c70ad
pasham Revanth <pashamrevanth541@gmail.com>
2026-03-22 12:40:19 +0530

    removed serverless packges

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,6 +1,12 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(process.env.DATABASE_URL!, {
+const DATABASE_URL = process.env.DATABASE_URL;
+
+if (!DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+export const sequelize = new Sequelize(DATABASE_URL, {
   dialect: "postgres",
   logging: false,
   dialectOptions: {
